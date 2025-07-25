723219f735fada6eabb9920a558526b8da4075b7
Impasta2048 <139719703+Coder20488@users.noreply.github.com>
2025-07-25 20:12:09 +0000

    found min and max of audience ratings and seen how my movie compares to these

--- main.py
+++ main.py
@@ -10,23 +10,31 @@ pd.set_option('max_colwidth', None)
 movieData = pd.read_csv('./rotten_tomatoes_movies.csv')
 favMovie = "Devdas"
 print('My favorite movie is ' + favMovie + "!")
-print(movieData.head())
+
 
 #Part 3 Investigate the data
+#print(movieData.head())
+#print(movieData["movie_title"])
 
 #Part 4 Filter data
 print("\nThe data for my favorite movie is:\n")
 #Create a new variable to store your favorite movie information
+favMovieBooleanList = movieData["movie_title"] == favMovie
+favMovieData = movieData.loc[favMovieBooleanList]
+print(favMovieData)
 
 print("\n\n")
 
+romanceMovieBooleanList = movieData["genres"].str.contains("Romance")
+romanceMovieData = movieData.loc[romanceMovieBooleanList]
 #Create a new variable to store a new data set with a certain genre
 
-numOfMovies = 0
+
+numOfMovies = romanceMovieData.shape[0]
 
 print("We will be comparing " + favMovie +
-      " to other movies under the genre [X] in the data set.\n")
-print("There are " + str(numOfMovies) + " movies under the category [X].")
+      " to other movies under the genre Romance in the data set.\n")
+print("There are " + str(numOfMovies) + " movies under the category Romance.")
 
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 input("Press enter to see more information about how " + favMovie +
@@ -34,28 +42,28 @@ input("Press enter to see more information about how " + favMovie +
 
 #Part 5 Describe data
 #min
-min = 0
+min = romanceMovieData["audience_rating"].min()
 print("The min audience rating of the data set is: " + str(min))
-print(favMovie + " is rated [X] points higher than the lowest rated movie.")
+print(favMovie + " is rated 79 points higher than the lowest rated movie.")
 print()
 
 #find max
-max = 0
+max = romanceMovieData["audience_rating"].max()
 print("The max audience rating of the data set is: " + str(max))
-print(favMovie + " is rated [X] points lower than the highest rated movie.")
+print(favMovie + " is rated 11 points lower than the highest rated movie.")
 print()
 
 #find mean
-mean = 0
+mean = romanceMovieData["audience_rating"].mean()
 print("The mean audience rating of the data set is: " + str(mean))
 print(favMovie +
-      " [is higher than/lower than/the same as] the mean movie rating.")
+      " is higher than the mean movie rating.")
 
 #find median
-median = 0
+median = romanceMovieData["audience_rating"].median()
 print("The median audience rating of the data set is: " + str(median))
 print(favMovie +
-      " [is higher than/lower than/the same as] the median movie rating.")
+      " is higher than the median movie rating.")
 
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 input("Press enter to see data visualizations.\n")
